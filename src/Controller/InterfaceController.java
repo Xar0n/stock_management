8642d3ed5035cd3fb413636811d158c874c9d18d
Xar0n <dima.dima.maksimov1998@mail.ru>
2021-06-12 19:53:03 +0800

    Иконки, получение некоторых данных с форм

--- src/Controller/InterfaceController.java
+++ src/Controller/InterfaceController.java
@@ -9,6 +9,8 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class InterfaceController extends Controller {
@@ -99,16 +101,16 @@ public class InterfaceController extends Controller {
     private Button btnBuyNew;
 
     @FXML
-    private ComboBox<?> cbReceiverN;
+    private ComboBox<String> cbReceiverN;
 
     @FXML
-    private ComboBox<?> cbStorageN;
+    private ComboBox<String> cbStorageN;
 
     @FXML
-    private ComboBox<?> cbStorageE;
+    private ComboBox<String> cbStorageE;
 
     @FXML
-    private ComboBox<?> cbProductE;
+    private ComboBox<String> cbProductE;
 
     @FXML
     private TextField tfCountPE;
@@ -125,10 +127,10 @@ public class InterfaceController extends Controller {
 
     //Sell
     @FXML
-    private ComboBox<?> cbStorageSell;
+    private ComboBox<String> cbStorageSell;
 
     @FXML
-    private ComboBox<?> cbProductSell;
+    private ComboBox<String> cbProductSell;
 
     @FXML
     private TextField tfCountSell;
@@ -209,11 +211,31 @@ public class InterfaceController extends Controller {
 
     @FXML
     void handleSell(ActionEvent event) {
-
+        if (event.getSource() == btnSell) {
+            //cbStorageSell
+            //cbProductSell
+            //cbSupplierSell
+            String count = tfCountSell.getText();
+            String price = tfPriceSell.getText();
+            LocalDate date = dateSell.getValue();
+        }
     }
 
     @FXML
     void handleBuy(ActionEvent event) {
+        if (event.getSource() == btnBuyNew) {
+            //cbStorageN
+            //cbReceiverN
+            String name = tfNameN.getText();
+            String count = tfCountPN.getText();
+            String price = tfPriceN.getText();
+            LocalDate date = dateBuyN.getValue();
+        } else if (event.getSource() == btnBuyExist) {
+            //cbStorageE
+            //cbProductE
+            String count = tfCountPE.getText();
+            LocalDate date = dateBuyE.getValue();
+        }
 
     }
 
@@ -224,22 +246,41 @@ public class InterfaceController extends Controller {
             pnlMain.setStyle(backgroundColor);
             pnlMain.toFront();
         } else if (event.getSource() == btnStorage) {
+
+
+
             pnlStorage.setStyle(backgroundColor);
             pnlStorage.toFront();
         } else if (event.getSource() == btnSupplier) {
+
+
+
+
             pnlSupplier.setStyle(backgroundColor);
             pnlSupplier.toFront();
         } else if (event.getSource() == btnReceiver) {
+
+
+
+
             pnlReceiver.setStyle(backgroundColor);
             pnlReceiver.toFront();
         } else if (event.getSource() == btnBuyPage) {
+            cbStorageN.valueProperty().set(null);
+            cbReceiverN.valueProperty().set(null);
+            cbStorageE.valueProperty().set(null);
+            cbProductE.valueProperty().set(null);
+
+
             pnlBuy.setStyle(backgroundColor);
             pnlBuy.toFront();
         } else if (event.getSource() == btnSellPage) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Исключение");
-            alert.setContentText("Продажа");
-            alert.showAndWait();
+            cbStorageSell.valueProperty().set(null);
+            cbProductSell.valueProperty().set(null);
+            cbSupplierSell.valueProperty().set(null);
+
+
+
             pnlSell.setStyle(backgroundColor);
             pnlSell.toFront();
         } else if (event.getSource() == btnSignout) {
@@ -257,7 +298,11 @@ public class InterfaceController extends Controller {
     @FXML
     void handleAdd(ActionEvent event) {
         if (event.getSource() == btnAddStorage) {
-            StorageController.createWindow(event, "Добавить ", "/View/Storage/add.fxml", true);
+            StorageController.createWindow(event, "Добавить склад", "/View/Storage/add.fxml", true);
+        } else if (event.getSource() == btnAddSupplier) {
+            SupplierController.createWindow(event, "Добавить покупателя", "/View/Supplier/add.fxml", true);
+        } else if (event.getSource() == btnAddReceiver) {
+            ReceiverController.createWindow(event, "Добавить поставщика", "/View/Receiver/add.fxml", true);
         }
     }
 
@@ -265,7 +310,11 @@ public class InterfaceController extends Controller {
     @FXML
     void handleDelete(ActionEvent event) {
         if (event.getSource() == btnDeleteStorage) {
-
+            String id = tfIdStorage.getText();
+        } else if (event.getSource() == btnDeleteReceiver) {
+            String id = tfIdReceiver.getText();
+        } else if (event.getSource() == btnDeleteSupplier) {
+            String id = tfIdSupplier.getText();
         }
     }
 
@@ -273,6 +322,10 @@ public class InterfaceController extends Controller {
     @FXML
     void handleEdit(ActionEvent event) {
         if (event.getSource() == btnEditStorage) {
+
+        } else if (event.getSource() == btnEditReceiver) {
+
+        } else if (event.getSource() == btnEditSupplier) {
 
         }
     }
--- src/Controller/ReceiverController.java
+++ src/Controller/ReceiverController.java
@@ -1,12 +1,28 @@
 package Controller;
 
 import Base.Controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ReceiverController extends Controller {
+    @FXML
+    private TextField tfName;
 
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private TextField tfAddress;
+
+    @FXML
+    void add(ActionEvent event) {
+
+    }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
