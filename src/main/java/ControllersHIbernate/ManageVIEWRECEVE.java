e9278448c8709e1978d5dbc2ecc7b68ec409f749
TriDoger <urilevak@gmail.com>
2021-06-15 18:34:34 +0800

    Вывод просмотра продаж (в прошлый раз поставок)
    Добавил формирование просмотра RECEVE

--- src/main/java/ControllersHIbernate/ManageVIEWRECEVE.java
+++ src/main/java/ControllersHIbernate/ManageVIEWRECEVE.java
@@ -1,27 +1,23 @@
 package ControllersHIbernate;
 
 import Base.ControllerHIbernate;
-import POJO.Recevier;
-import POJO.Suppliers;
+import POJO.VIEW_RECEVE;
 import POJO.VIEW_SUPPLY;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import org.hibernate.HibernateException;
-import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
-import org.hibernate.transform.AliasToBeanResultTransformer;
 
 import java.util.Iterator;
 import java.util.List;
 
-public class ManageVIEWSUPPLY extends ControllerHIbernate {
-    public ManageVIEWSUPPLY() {
+public class ManageVIEWRECEVE extends ControllerHIbernate {
+    public ManageVIEWRECEVE() {
     }
-
 //    public static void main(String[] args) { //:TODO удалить тестовый блок
 //
 //        try {
@@ -34,26 +30,26 @@ public class ManageVIEWSUPPLY extends ControllerHIbernate {
 //            System.err.println("Failed to create sessionFactory object." + ex);
 //            throw new ExceptionInInitializerError(ex);
 //        }
-//        ManageVIEWSUPPLY mv = new ManageVIEWSUPPLY();
-//        mv.selectAll();
+//        ManageVIEWRECEVE MV = new ManageVIEWRECEVE();
+//        MV.selectAll();
 //    }
-    public ObservableList<VIEW_SUPPLY> selectAll(){
-        Session session = factory.openSession();
-        Transaction tx = null;
-        ObservableList<VIEW_SUPPLY> list = FXCollections.observableArrayList();
-        try {
-            tx = session.beginTransaction();
-            List view_supply1 = session.createQuery("FROM VIEW_SUPPLY").list();
-            for (Iterator iterator = view_supply1.iterator(); iterator.hasNext();) {
-                list.add((VIEW_SUPPLY) iterator.next());
+        public ObservableList<VIEW_RECEVE> selectAll(){
+            Session session = factory.openSession();
+            Transaction tx = null;
+            ObservableList<VIEW_RECEVE> list = FXCollections.observableArrayList();
+            try {
+                tx = session.beginTransaction();
+                List viewReceve = session.createQuery("FROM VIEW_RECEVE").list();
+                for (Iterator iterator = viewReceve.iterator(); iterator.hasNext();) {
+                    list.add((VIEW_RECEVE) iterator.next());
+                }
+                tx.commit();
+            } catch (HibernateException e) {
+                if (tx!=null) tx.rollback();
+                e.printStackTrace();
+            } finally {
+                session.close();
             }
-            tx.commit();
-        } catch (HibernateException e) {
-            if (tx!=null) tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
+            return list;
         }
-        return list;
-    }
 }
